59a13545bba173050e29c99c12f5661d1ac36136
tfurr01 <tfurr01@gmail.com>
2021-05-12 16:23:59 -0400

    add some methods to Board

--- board.rb
+++ board.rb
@@ -2,15 +2,29 @@
 
 # class for the board and spaces
 class Board
-  @spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  @board = <<~HEREDOC
-
-     #{@spaces[0]} | #{@spaces[1]} | #{@spaces[2]}
-    ---+---+---
-     #{@spaces[3]} | #{@spaces[4]} | #{@spaces[5]}
-    ---+---+---
-     #{@spaces[6]} | #{@spaces[7]} | #{@spaces[8]}
-
-  HEREDOC
   attr_accessor :spaces
+
+  def initialize
+    @spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def draw
+    puts <<~HEREDOC
+
+      #{@spaces[0]} | #{@spaces[1]} | #{@spaces[2]}
+      ---+---+---
+      #{@spaces[3]} | #{@spaces[4]} | #{@spaces[5]}
+      ---+---+---
+      #{@spaces[6]} | #{@spaces[7]} | #{@spaces[8]}
+
+    HEREDOC
+  end
+
+  def add_move(move, player_symbol)
+    @spaces[move - 1] = player_symbol
+  end
+
+  def validate(move)
+    puts 'Invalid move! Please choose an empty space.' unless move.between?(1, 9)
+  end
 end
